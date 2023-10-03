ed06365949aafb04983507512358c037914421c2
FUKUI AKANE <akane.2702@gmail.com>
2023-10-03 10:31:32 +0200

    creating index.js, package.json for testing  module chalk

---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+
+console.log(chalk.bgMagenta("Caroline"));
+console.log(chalk.bgGreenBright("Sissis"));
+console.log(chalk.bgGrey("Ange"));
+console.log(chalk.bgWhite("Karim"));
